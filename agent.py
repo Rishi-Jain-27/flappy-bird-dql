abd7622d3ba5b496775ad76ca1d483ea0828f385
Rishi Jain <27rishijainpersonalemail@gmail.com>
2026-06-02 21:00:05 -0400

    Fixed bugs in training

--- agent.py
+++ agent.py
@@ -8,6 +8,7 @@ import torch
 from torch import nn
 import flappy_bird_gymnasium
 import gymnasium
+from gymnasium.spaces import Discrete
 from dqn import DQN
 from experience_replay import ReplayMemory
 import itertools
@@ -19,7 +20,8 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import argparse
 import itertools
-
+import numpy as np
+from torch.optim import Optimizer
 
 # for printing date and time
 DATE_FORMAT = "%m-%d %H:%M:%S"
@@ -37,6 +39,7 @@ class Agent:
             all_hyperparameter_sets = yaml.safe_load(file)
             hyperparameters = all_hyperparameter_sets[hyperparameter_set]
             # print(hyperparameters)
+        self.hyperparameter_set = hyperparameter_set
         
         self.env_id = hyperparameters['env_id']
         self.network_sync_rate = hyperparameters['network_sync_rate']
@@ -53,7 +56,7 @@ class Agent:
         self.env_make_params = hyperparameters.get('env_make_params', {})
         
         self.loss_fn = nn.MSELoss()
-        self.optimizer = None # NN optimizer, initialize later
+        self.optimizer: Optimizer | None = None  # init later at line 58
 
         # Path to run info
         self.LOG_FILE = os.path.join(RUNS_DIR, f'{self.hyperparameter_set}.log')
@@ -66,7 +69,7 @@ class Agent:
         last_graph_update_time = start_time
 
         log_message = f"{start_time.strftime(DATE_FORMAT)}: Training starting"
-        print(log_message)
+        print(log_message, flush=True)
         with open(self.LOG_FILE, 'w') as file:
             file.write(log_message + '\n')
 
@@ -77,9 +80,12 @@ class Agent:
         env = gymnasium.make(self.env_id, render_mode="human" if render else None, **self.env_make_params)
     
         # num of states is the dim of the input layer
+        assert env.observation_space.shape is not None
         num_states = env.observation_space.shape[0]
+
         # num possible actions
-        num_actions = env.action_space.n
+        assert isinstance(env.action_space, Discrete)
+        num_actions = int(env.action_space.n)
 
         # list to keep track of rewards per episode
         rewards_per_episode = []
@@ -94,7 +100,7 @@ class Agent:
             memory = ReplayMemory(self.replay_memory_size) # we'll make it dynamic later
 
             # Create target network and make it identical to policy network
-            target_dqn = DQN(num_staes, num_actions, self.fc1_nodes).to(device)
+            target_dqn = DQN(num_states, num_actions, self.fc1_nodes).to(device)
             target_dqn.load_state_dict(policy_dqn.state_dict())
 
             # Create policy network optimizer
@@ -136,7 +142,7 @@ class Agent:
                 new_state, reward, terminated, _, info = env.step(action.item())
 
                 # Accumulate reward for this episode
-                episode_reward += reward
+                episode_reward += float(reward)
 
                 # Convert new state and reward to tensors
                 new_state = torch.tensor(new_state, dtype=torch.float, device=device)
@@ -159,7 +165,7 @@ class Agent:
             if is_training:
                 if episode_reward > best_reward:
                     log_message = f"{datetime.now().strftime(DATE_FORMAT)}: New best reward {episode_reward:0.1f}"
-                    print(log_message)
+                    print(log_message, flush=True)
 
                     with open(self.LOG_FILE, 'a') as file:
                         file.write(log_message + '\n')
@@ -188,59 +194,60 @@ class Agent:
                         target_dqn.load_state_dict(policy_dqn.state_dict())
                         step_count = 0
 
-def save_graph(self, rewards_per_episode, epsilon_history):
-    # Save plots
-    fig = plt.figure(1)
+    def save_graph(self, rewards_per_episode, epsilon_history):
+        # Save plots
+        fig = plt.figure(1)
 
-    # Plot average rewards (Y) vs episodes (X)
-    mean_rewards = np.zeros(len(rewards_per_episode))
-    for x in range(len(mean_rewards)):
-        mean_rewards[x] = np.mean(rewards_per_episode[max(0, x - 99):(x + 1)])
-    
-    plt.subplot(1, 2, 1)
+        # Plot average rewards (Y) vs episodes (X)
+        mean_rewards = np.zeros(len(rewards_per_episode))
+        for x in range(len(mean_rewards)):
+            mean_rewards[x] = np.mean(rewards_per_episode[max(0, x - 99):(x + 1)])
+        
+        plt.subplot(1, 2, 1)
 
-    plt.xlabel('Episodes')
-    plt.ylabel('Mean rewards')
-    plt.plot(mean_rewards)
+        plt.xlabel('Episodes')
+        plt.ylabel('Mean rewards')
+        plt.plot(mean_rewards)
 
-    # Plot epsilon decay (Y) vs episodes (X)
-    plt.subplot(1, 2, 2)
-    plt.xlabel('Time steps')
-    plt.ylabel('Epsilon Decay')
-    plt.plot(epsilon_history)
+        # Plot epsilon decay (Y) vs episodes (X)
+        plt.subplot(1, 2, 2)
+        plt.xlabel('Time steps')
+        plt.ylabel('Epsilon Decay')
+        plt.plot(epsilon_history)
 
-    plt.subplots_adjust(wspace=1.0, hspace=1.0)
+        plt.subplots_adjust(wspace=1.0, hspace=1.0)
 
-    # Save plots
-    fig.savefig(self.GRAPH_FILE)
-    plt.close(fig)
+        # Save plots
+        fig.savefig(self.GRAPH_FILE)
+        plt.close(fig)
 
-def optimize(self, mini_batch, policy_dqn, target_dqn):
-    # Tranpose the list of experiences and separate each element
-    states, actions, new_states, rewards, terminations = zip(*mini_batch)
+    def optimize(self, mini_batch, policy_dqn, target_dqn):
+        # Tranpose the list of experiences and separate each element
+        states, actions, new_states, rewards, terminations = zip(*mini_batch)
 
-    # Stack tensors to create batch tensors
-    # tensor ([[1, 2, 3]]) -> tensor([[1, 2, 3, ...], [4, 5, 6, ...], ...])
-    states = torch.stack(states)
-    actions = torch.stack(actions)
-    new_states = torch.stack(new_states)
-    rewards = torch.stack(rewards)
-    terminations = torch.tensor(terminations).float().to(device)
+        # Stack tensors to create batch tensors
+        # tensor ([[1, 2, 3]]) -> tensor([[1, 2, 3, ...], [4, 5, 6, ...], ...])
+        states = torch.stack(states)
+        actions = torch.stack(actions)
+        new_states = torch.stack(new_states)
+        rewards = torch.stack(rewards)
+        terminations = torch.tensor(terminations).float().to(device)
 
-    with torch.inference_mode():
-        # Calculate target q values (expected return)
-        target_q = rewards + (1 - terminations) * self.discount_factor_g * target_dqn(new_states).max(dim=1)[0]
+        with torch.inference_mode():
+            # Calculate target q values (expected return)
+            target_q = rewards + (1 - terminations) * self.discount_factor_g * target_dqn(new_states).max(dim=1)[0]
 
-    # Calculate q values from current policy network
-    current_q = policy_dqn(states).gather(dim=1, index=actions.unsqueeze(dim=1)).squeeze(dim=1)
+        # Calculate q values from current policy network
+        current_q = policy_dqn(states).gather(dim=1, index=actions.unsqueeze(dim=1)).squeeze(dim=1)
 
-    # Compute loss for the whole minibatch
-    loss = self.loss_fn(current_q, target_q)
+        # Compute loss for the whole minibatch
+        loss = self.loss_fn(current_q, target_q)
 
-    # Optimize the model
-    self.optimizer.zero_grad()
-    loss.backward()
-    self.optimizer.step()
+        # Optimize the model
+        assert self.optimizer is not None
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
 
 if __name__ == '__main__':
